720bdc813591442d3794ab0e8fe30042de7a960a
Afif <afifkabir10@gmail.com>
2024-03-27 16:14:06 +0600

    12

---
+++ Floor and exponent command.py
@@ -0,0 +1,17 @@
+'''
++ denotes sum
+- denotes subtraction
+* denotes multiplication
+/ denotyes division
+// denotes floor
+** denotes exponent 
+'''
+a=4
+b=3
+print(a+b)
+print(a-b)
+print(a*b)
+print(a/b)
+print(a%b)
+print(a//b)
+print(a**b)
