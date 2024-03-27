1a80ba5914447989f1003699ff4fcb3a7d327821
Afif <afifkabir10@gmail.com>
2024-03-27 15:18:11 +0600

    hi

---
+++ Comment & Backclashchar.py
@@ -0,0 +1,9 @@
+#is used for one line comment
+'''
+cotation for a few line comments
+\n for new line
+\t for tab
+\" for printing double cotation
+\' for printing single cotation
+'''
+print("Comment & Backclashcharacter commands")
