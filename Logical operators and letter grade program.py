192557a7c87b5bc8596e2c47c006f4d8f92d63c7
Afif <afifkabir10@gmail.com>
2024-03-28 13:21:27 +0600

    ALLAH

---
+++ Logical operators and letter grade program.py
@@ -0,0 +1,15 @@
+num1=10
+num2=-5
+num3=11
+if num1>num2 and num1>num3 :
+    print(num1)
+elif num2>num3 and num2>num1 :
+    print(num2)
+else :
+    print(num3)
+#Letter grading
+mark=85
+if 80<= mark <= 100 :
+    print("A+")
+else :
+    print("Not A+")
